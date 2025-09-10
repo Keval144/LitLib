0b99bbe7d5fce8aabdf1083410c8c597a1167022
keval kansagra <kevalm144@gmail.com>
2025-09-10 17:26:56 +0530

    Build bug fix

--- app/ItemEdit/[id]/page.tsx
+++ app/ItemEdit/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: any } }) {
   redirect(`/admin/items/${params.id}/edit`);
 }
-
